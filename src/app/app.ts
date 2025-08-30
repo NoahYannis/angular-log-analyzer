e4405b9495b8e1e2cd6c40429e1ac5afde0fce1d
Noah Yannis Guderjahn <noah.guderjahn@gmx.de>
2025-08-30 19:12:12 +0200

    ziemlich hübsch

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,8 +7,10 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { DatePipe } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
@@ -22,13 +24,13 @@ import { DatePipe } from '@angular/common';
     MatInputModule,
     MatIconModule,
     MatTableModule,
+    MatButtonModule,
+    MatTooltipModule,
     DatePipe,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-
-
 export class App {
   protected readonly title = signal('angular-log-analyzer');
 
@@ -37,79 +39,87 @@ export class App {
     {
       date: '2025-08-30',
       time: '10:45:12',
-      level: 'INFO',
+      level: 'I',
       source: 'DAL',
       message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
       date: '2025-08-30',
       time: '10:46:02',
-      level: 'WARNING',
+      level: 'W',
       source: 'FW',
       message: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'ERROR',
+      level: 'E',
       source: 'CAD',
-      message: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      message:
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'VERBOSE',
+      level: 'V',
       source: 'CAD',
-      message: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      message:
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'INFO',
+      level: 'I',
       source: 'CAD',
-      message: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      message:
+        'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'WARNING',
+      level: 'W',
       source: 'CAD',
-      message: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
+      message:
+        'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'ERROR',
+      level: 'E',
       source: 'CAD',
-      message: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti.',
+      message:
+        'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'VERBOSE',
+      level: 'V',
       source: 'CAD',
       message: 'Et harum quidem rerum facilis est et expedita distinctio.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'INFO',
+      level: 'I',
       source: 'CAD',
-      message: 'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat.',
+      message:
+        'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'WARNING',
+      level: 'W',
       source: 'CAD',
-      message: 'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae.',
+      message:
+        'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae.',
     },
-       {
+    {
       date: '2025-08-30',
       time: '10:47:33',
-      level: 'ERROR',
+      level: 'E',
       source: 'CAD',
-      message: 'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis.',
+      message:
+        'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis.',
     },
   ];
 }
